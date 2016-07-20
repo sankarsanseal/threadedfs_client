a9651261c28486431e9726f0387633e5ad35ff54
Sankarsan Seal <sankarsanseal@icloud.com>
2016-07-20 13:27:37 +0530

    queue is to be added

--- threadedfs_client/main.c
+++ threadedfs_client/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -20,20 +21,57 @@ int userid;
 char  path_to_server[1024];
 char  pwd[1024];
 
+typedef struct instruction
+{
+    int instruct_code;
+    int userid;
+    char pwd[1024];
+    
+};
+
+
 void setuserid()
 {
     printf("Enter the user ID for filesystem access:");
     scanf("%d",&userid);
-    sprintf(pwd,"/");
+    strcpy(pwd,"/");
+    
 }
 
 void printpwd()
 {
+    int choice;
     printf("Present working directory of user id %d: %s\n",userid,pwd);
+    printf("Want to see list?\n1.Yes\n2.No\n");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        int
+        write(serverfifo,
+    }
 }
+
+void changedir()
+{
+    char dirname[256];
+    printf("Enter the directory name:");
+    scanf("%[^\n]s",dirname);
+    write(
+    
+}
+
+void makedir()
+{
+    char dirname[256];
+    printf("Enter the name of new directory:");
+    scanf("%[^\n]s",dirname);
+}
+
+
 
 void menu()
 {
+    
     int c=1;
     while(c!=0)
     {
@@ -47,6 +85,8 @@ void menu()
         printf("8.Rename Directory or File\n");
         printf("9.Delete File(rm)\n");
         printf("0.For exit the client.\n");
+        printf("Enter your choice:");
+        scanf("%d",&c);
         switch(c)
         {
             case 1:
@@ -55,6 +95,16 @@ void menu()
             case 2:
                 printpwd();
                 break;
+            case 3:
+                changedir();
+                break;
+            case 4:
+                makedir();
+                break;
+                
+            case 0:
+                printf("Exiting...\n");
+                
                 
                 
         }
