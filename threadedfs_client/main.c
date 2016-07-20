2f1b9be2c4c4957f1aa4807d8f9b1d6e9b6fe637
Sankarsan Seal <sankarsanseal@icloud.com>
2016-07-20 11:04:16 +0530

    Client and main server are modified.

--- threadedfs_client/main.c
+++ threadedfs_client/main.c
@@ -7,9 +7,35 @@
 //
 
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int serverfifo;
+    int clientfifo;
+    int server_id
+    int client_pid;
+    char * path_to_server[1024];
+    
+    if(argc!=2)
+    {
+        printf("usage:%s <server_pid>");
+        return 0;
+    }
+    else
+    {
+        client_pid=getpid();
+        sprintf(path_to_server,"/tmp/server.%d",);
+        if(!access(path_to_server))
+        {
+            
+        }
+        else
+        {
+            printf("Server does not")
+        }
+        
+    }
     return 0;
 }
